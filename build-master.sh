25bf36c03abaf069ddab24326d70bc41565e769c
NBPRV <naelbadawi58@gmail.com>
2026-05-22 10:03:22 +0200

    Create build-master.sh

---
+++ build-master.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -e
+
+OUT="playlists/master.m3u"
+TMP="$(mktemp)"
+
+mkdir -p playlists
+
+echo "#EXTM3U" > "$OUT"
+
+while read -r src; do
+  [ -z "$src" ] && continue
+
+  if [[ "$src" == local:* ]]; then
+    file="${src#local:}"
+    tail -n +2 "$file" >> "$TMP"
+  else
+    curl -L --silent "$src" | tail -n +2 >> "$TMP"
+  fi
+
+  echo "" >> "$TMP"
+done < sources.txt
+
+awk '
+  /^#EXTINF/ { info=$0; next }
+  /^https?:/ {
+    if (!seen[$0]++) {
+      print info
+      print $0
+      print ""
+    }
+  }
+' "$TMP" >> "$OUT"
+
+rm "$TMP"
+echo "Built $OUT"
